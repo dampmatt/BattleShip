957e4a268b024708947d74978628bb8d1507269b
Utkarsh <utkarshchoudhary405@gmail.com>
2025-02-06 12:38:08 +0530

    game completed

--- src/game.js
+++ src/game.js
@@ -7,22 +7,33 @@ export class game {
     COMPUTER_TURN: "COMPUTER_TURN",
     GAME_OVER: "GAME_OVER",
   };
-
+  name;
   currentState;
   player1;
   computer;
   winner;
 
   constructor(name) {
+    this.name = name;
     this.player1 = new gameBoard(name);
     this.computer = new gameBoard("computer");
     this.currentState = this.states.INITIALIZING;
     this.winner = null;
   }
 
+  playerRandomize() {
+    if (this.currentState !== "INITIALIZING") return 0;
+    // console.log(123);
+    this.player1 = new gameBoard(this.name);
+    this.computer = new gameBoard("computer");
+    this.placePlayerShipsRandomly();
+    return 1;
+  }
+
   startGame() {
     if (this.currentState !== "INITIALIZING") return;
     this.generateComputerShipCoordinates();
+    // this.generatePlayerShipCoordinates();
     this.player1.initiateGame();
     this.computer.initiateGame();
     this.currentState = this.states.PLAYER_TURN;
@@ -45,49 +56,68 @@ export class game {
     return x;
   }
 
+  placePlayerShipsRandomly() {
+    var z = 1;
+    for (let i = 1; i <= 4; i++) {
+      var x = Math.floor(Math.random() * 7);
+      var y = Math.floor(Math.random() * 7);
+      var dir = Math.floor(Math.random() * 2);
+      var result = this.player1.placeShips([x, y], i, dir);
+      if (!(result === 1)) {
+        i--;
+        z--;
+      }
+      z++;
+    }
+    return z;
+  }
+
   computerTurn() {
     if (this.currentState !== this.states.COMPUTER_TURN) return;
     var x = Math.floor(Math.random() * 7);
     var y = Math.floor(Math.random() * 7);
+    var id = this.player1.board[x][y];
     var result = this.player1.missileHit(x, y);
-    if (result === -1) {
+    if (result === 0) {
       //blank but correct shot, next turn
       this.currentState = this.states.PLAYER_TURN;
-    } else if (result === 1) {
+    } else if (result === -1) {
       //incorrect shot, do again
-      this.computerTurn();
-    } else {
-      if (result === 0) {
-        //correct shot. Do agains
-        this.computerTurn();
-      } else if (result === 2) {
-        //correct shot. Game Won
-        this.endGame("computer");
-      }
+      var result = this.computerTurn();
+      x = result[1];
+      y = result[2];
+      id = result[3];
+
+      result = result[0];
     }
+    if (result === 2) {
+      //correct shot. Game Won
+      this.endGame("computer");
+    }
+    return [result, x, y, id];
   }
 
   humanTurn(x, y) {
-    if (this.currentState !== this.states.PLAYER_TURN) return;
-
+    if (this.currentState !== this.states.PLAYER_TURN) return -1;
+    var id = this.computer.board[x][y];
     var result = this.computer.missileHit(x, y);
     if (result === 0) {
-      this.currentTurn = "computer";
-      return 0;
+      this.currentState = this.states.COMPUTER_TURN;
+      result = 0;
       // this.computerTurn();
       //blank but correct shot, next turn
     } else {
       if (result === 2) {
         //correct shot. Game Won
         this.endGame("player1");
-        return 2;
       }
     }
-    return result;
+    return [result, id];
 
     //returns 0-> if shot missed
     //returns 1->shot placed
-    //returns -1 -> if incorrect shot
+    //returns -1 -> if incorrect shot\
+    //returns 2->if game finish
   }
 
   endGame(winner) {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,128 @@
+import "./styles.css";
+import { game } from "./game";
 
-import "./styles.css";  
+const $playerGrid = document.querySelector("#player-grid");
+const $computerGrid = document.querySelector("#computer-grid");
+const $startGame = document.querySelector("#start-game-btn");
+const $randomize = document.querySelector("#randomize-btn");
+const $startNewGame = document.querySelector("#start-new-game-btn");
+const Game = new game("player");
 
+$startGame.addEventListener("click", startGame);
+$randomize.addEventListener("click", randomize);
+$startNewGame.addEventListener("click", refresh);
+
+function refresh() {
+  window.location.reload();
+}
+
+function randomize() {
+  var result = Game.playerRandomize();
+  updatePlayerBoard();
+}
+
+function startGame() {
+  Game.startGame();
+}
+
+function updatePlayerBoard() {
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+      const currCell = document.querySelector("#cell-" + i + "" + j);
+      currCell.classList.remove("occupied");
+      if (Game.player1.board[i][j] > 0) {
+        currCell.classList.add("occupied");
+      }
+    }
+  }
+}
+
+function renderPlayerBoard() {
+  var result = Game.computerTurn();
+  const cell = document.querySelector("#cell-" + result[1] + "" + result[2]);
+  // console.log(result);
+  if (result[0] === 1 || result[0] === 2) {
+    cell.classList.remove("occupied");
+    cell.classList.remove("cells");
+    cell.classList.add("hit");
+    var id = 4 - result[3];
+    const ship = document.querySelector("#player-list").children[id];
+
+    var n = 0;
+    Array.from(ship.children).forEach((cell) => {
+      if (cell.classList.contains("unhit") && n === 0) {
+        cell.classList.remove("unhit");
+        cell.classList.add("hit");
+        n = 1;
+      }
+    });
+    if (result[0] === 1) {
+      renderPlayerBoard();
+    }
+    if (result[0] === 2) {
+      const heading = document.querySelector("#winner-headline");
+      heading.innerHTML = "<h1>Computer Wins<h1>";
+      heading.style.visibility = "visible";
+    }
+  } else if (result[0] === 0) {
+    // console.log(cell);
+    cell.classList.remove("cells");
+    cell.classList.add("miss");
+  }
+}
+
+function renderGameState(event) {
+  const cell = event.target;
+  const idParts = cell.id.split("-");
+  const r = Number(idParts[1][0]);
+  const c = Number(idParts[1][1]);
+  var result = Game.humanTurn(r, c);
+  console.log(result);
+  if (result[0] === 1 || result[0] === 2) {
+    cell.classList.remove("cells");
+    cell.classList.add("hit");
+
+    var id = 4 - result[1];
+    const ship = document.querySelector("#computer-list").children[id];
+
+    var n = 0;
+    Array.from(ship.children).forEach((cell) => {
+      if (cell.classList.contains("unhit") && n === 0) {
+        cell.classList.remove("unhit");
+        cell.classList.add("hit");
+        n = 1;
+      }
+    });
+    if (result[0] === 2) {
+      // console.log(result);
+      const heading = document.querySelector("#winner-headline");
+      heading.innerHTML = "<h1>PLayer Wins<h1>";
+      heading.style.visibility = "visible";
+    }
+  } else if (result[0] === 0) {
+    cell.classList.remove("cells");
+    cell.classList.add("miss");
+
+    renderPlayerBoard();
+  }
+}
+
+for (let i = 0; i < 7; i++) {
+  for (let j = 0; j < 7; j++) {
+    const gridComp = document.createElement("div");
+    const gridPlayer = document.createElement("div");
+    gridComp.classList.add("cells");
+    gridComp.id = "cell-" + i + "" + j;
+    gridPlayer.classList.add("cells");
+    gridPlayer.id = "cell-" + i + "" + j;
+    $playerGrid.appendChild(gridComp);
+    $computerGrid.appendChild(gridPlayer);
+  }
+}
+
+Array.from($computerGrid.children).forEach((cell) => {
+  cell.addEventListener("click", renderGameState);
+});
+
+Game.placePlayerShipsRandomly();
+updatePlayerBoard();
